8a21407b9355078402aac74f0c2ac70719883269
Gourav Kadam <gouravkadam49@gmail.com>
2021-11-28 10:28:31 +0530

    working with classes

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModel = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+
+for (let i = 0 ; i < btnsOpenModal.length ; i++)
+  btnsOpenModal[i].addEventListener('click' , function(){
+    // console.log('button clicked' , [i]);
+    // document.querySelector('.hidden').style.display = 'block' ;
+    // document.body.style.filter = 'blur(4px)';
+    // document.querySelector('.hidden').style.filter = 'none' ;
+    // document.querySelector('.close-modal').addEventListener('click' , function(){
+    //   document.querySelector('.hidden').style.display = 'none' ;
+    //   document.querySelector('.hidden').style.fliter = 'none' ;
+    //   document.body.style.filter = 'none' ;
+    // });
+    // document.querySelector('.close-modal').style.fliter = 'none' ;
+
+    console.log('button clicked' , [i]);
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+    model.style.display = 'block';
+
+  });
+
+  const closeModel = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  }
+  btnCloseModel.addEventListener('click' , closeModel);
+
+  overlay.addEventListener('click', closeModel);
